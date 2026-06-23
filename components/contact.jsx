cb29263fc767adb5e9055edc0aee23e4836f49b1
Zeeshan <jisankhan@bhutanigroup.com>
2026-06-23 16:22:08 +0530

    changed number

--- components/contact.jsx
+++ components/contact.jsx
@@ -349,7 +349,7 @@ export default function ContactUsPage() {
                 </p>
 
                 <p className="text-gray-600 text-sm mt-1">
-                  +91 9876543210
+                  +91 8182837993
                 </p>
               </div>
             </div>
